588ae38822791cd89e7d95a6a71d7c06b8423041
yannzimmer <yann.zimmer.fr@gmail.com>
2015-11-17 14:05:31 +0100

    Step 4

--- lib/app.jsx
+++ lib/app.jsx
@@ -5,15 +5,16 @@ var React = require('react');
 var Style = {height : "100px", width : "100px"};
 
 var Box = React.createClass({
-  /**
-   * Render a HTML button
-   * @return {ReactElement}
-   */
+  getInitialState: function(){
+    return {
+      value: 'X'
+    }
+  },
   'render': function onRender () {
     return (
-      <button style={Style}>{this.props.value}</button>
+      <button style={Style}>{this.state.value}</button>
     );
   }
 });
 
-React.render(<Box value='X'/>, document.body);
+React.render(<Box/>, document.body);
