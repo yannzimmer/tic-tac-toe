88c6a388d9e4b36d2a4be9548c4130b8dc14d20c
yannzimmer <yann.zimmer.fr@gmail.com>
2015-11-17 14:24:53 +0100

    Step 5

--- lib/app.jsx
+++ lib/app.jsx
@@ -10,6 +10,22 @@ var Box = React.createClass({
       value: 'X'
     }
   },
+
+  componentWillMount: function(){
+      var _this = this;
+      this.timer = setInterval(function(){
+        var oldValue = _this.state.value;
+        var newValue = oldValue === 'X' ? 'O': 'X';
+        _this.setState({
+          value: newValue
+        });
+      }, 300);
+    },
+    
+    componentWillUnmount: function(){
+      clearInterval(this.timer);
+    },
+
   'render': function onRender () {
     return (
       <button style={Style}>{this.state.value}</button>
